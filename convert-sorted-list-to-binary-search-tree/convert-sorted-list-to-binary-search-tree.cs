343280fe0f718ca2602567070706ac5583ddd2bd
monilgandhi3 <87777360+monilgandhi3@users.noreply.github.com>
2021-09-20 11:37:26 -0700

    Time: 214 ms (6.06%), Space: 27.8 MB (38.79%) - LeetHub

--- convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cs
+++ convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cs
@@ -23,10 +23,21 @@
  * }
  */
 
-/*
-* 
-*
-**/
+    /**
+    * 1. Find the size of the list
+    * 2. This is very similar to inorder traversal. 
+    TIP if you want to get a sorted order from a Binary Search tree, use inorder traversal. This list represents inorder traversed tree
+    * 3. We do inorder traversal from start to end
+    * 4. Base condition will be when start > end return null. 
+    * 5. At every step our start and end keep getting closer. We do this by dividing the list into left and right subtree by calculating mid
+    * 6. For mid we need actual index. hence it will always be with respect to start i.e. start + ((end - start) / 2)
+    * 7. The left tree is everything from start, mid -1 and right subtree is mid+1 to end.
+    * 8. Remember to move the tracking node which will point to the current node in linked list.
+    *
+    * Time Complexity will be O(N) to traverse all the nodes
+    * Space complexity will be O(N) + O(N). O(N) is for the tree nodes and O(N) is for the space of the stack for building left and right subtree.
+    */
+
 public class Solution {
     private ListNode trackingNode;
     public TreeNode SortedListToBST(ListNode head) {
