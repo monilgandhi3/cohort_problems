522b758185fb9ff6e08593b0c5ac1dc70aa22abf
monilgandhi3 <87777360+monilgandhi3@users.noreply.github.com>
2022-01-05 09:57:28 -0800

    Time: 3 ms (34.46%), Space: 44.5 MB (5.29%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -18,51 +18,27 @@ class Node {
 */
 
 class Solution {
-    public List<List<Integer>> levelOrder(Node root) {
-        // start with root
-        // maintain list or height map
-        // add all the children to queue and to the result with the correct height map
-        // when processing the current element from the queue, how to know the height. 
-            // check the map. this could be a set. but set is unordered. so output will be off.
-            // need to maintain only current and next set. 
-        // another option is to poll the list until current level. 
-            // for instance in the beginning we will have 3,2,4. This is the current level. We visit all the child nodes till we empty the queue. Maintain a separate list. and then insert the list into the queue. This is bit more time complexity
-        // keep processing till no item in queue
-        // return height map
+public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ret = new LinkedList<>();
         
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
-        if(root == null) {
-            return results;
-        }
+        if (root == null) return ret;
         
         Queue<Node> queue = new LinkedList<>();
         
-        HashSet<Node> currentLevel = new HashSet<>();
-        queue.add(root);
-        currentLevel.add(root);
-        HashSet<Node> nextLevel = new HashSet<>();
+        queue.offer(root);
         
-        List<Integer> currentLevelResult = new ArrayList<>();
-        while(!queue.isEmpty()) {
-            Node currentNode = queue.remove();
-            if(nextLevel.contains(currentNode)) {
-                // we have reached the end of the level. add the current list to result. create new list
-                results.add(currentLevelResult);
-                currentLevelResult = new ArrayList<>();
-                
-                // reset current level and next level
-                currentLevel = nextLevel;
-                nextLevel = new HashSet<>();
+        while (!queue.isEmpty()) {
+            List<Integer> curLevel = new LinkedList<>();
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Node curr = queue.poll();
+                curLevel.add(curr.val);
+                for (Node c : curr.children)
+                    queue.offer(c);
             }
-            
-            currentLevelResult.add(currentNode.val);
-            currentLevel.remove(currentNode);
-            // add all the children
-            nextLevel.addAll(currentNode.children);
-            queue.addAll(currentNode.children);
+            ret.add(curLevel);
         }
         
-        results.add(currentLevelResult);
-        return results;
+        return ret;
     }
 }
