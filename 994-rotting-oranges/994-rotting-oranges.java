26fdacad4e19a2fc090af7010fd7fbb130fbd501
monilgandhi3 <87777360+monilgandhi3@users.noreply.github.com>
2022-01-20 11:01:47 -0800

    Time: 13 ms (7.82%), Space: 40 MB (14.21%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -11,7 +11,6 @@ class Solution {
         
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
-                System.out.println("Checking " + grid[i][j]);
                 if(grid[i][j] == 2){
                     rottenQueue.add(new RottenOrange(i, j));
                 } else if (grid[i][j] == 1) {
@@ -24,7 +23,6 @@ class Solution {
         int timeTaken = 0;
         while(!rottenQueue.isEmpty())
         {
-            System.out.println("processing queue");
             RottenOrange r = rottenQueue.poll();
             
             // down
@@ -37,7 +35,6 @@ class Solution {
             
             // left
             if(r.x-1 >= 0 && grid[r.x-1][r.y] == 1) {
-                System.out.println("Adding left");
                 justRotten.add(new RottenOrange(r.x-1, r.y));
                 grid[r.x-1][r.y] = 2; 
                 --freshOranges;
@@ -45,7 +42,6 @@ class Solution {
             
             // right
             if(r.x+1 < grid.length && grid[r.x+1][r.y] == 1) {
-                System.out.println("adding right");
                 justRotten.add(new RottenOrange(r.x+1, r.y));
                 grid[r.x+1][r.y] = 2;
                 --freshOranges;
@@ -53,7 +49,6 @@ class Solution {
             
             // up
             if(r.y-1 >= 0 && grid[r.x][r.y-1] == 1) {
-                System.out.println("Adding up");
                 justRotten.add(new RottenOrange(r.x, r.y-1));
                 grid[r.x][r.y-1] = 2;
                 --freshOranges;
@@ -62,9 +57,7 @@ class Solution {
             if(rottenQueue.isEmpty() && justRotten.size() > 0){
                 ++timeTaken;
                 rottenQueue.addAll(justRotten);
-                System.out.println("Adding new " + justRotten.size());
                 justRotten = new ArrayList<>();
-                System.out.println("------------");
             }
         }
         
